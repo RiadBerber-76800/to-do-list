6fc018fd20d7fccc2df1c151ad4f6d30815e87f7
RiadBerber-76800 <riadberber@gmail.com>
2022-06-20 10:57:21 +0200

    done-task

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
-document.getElementById("push").onclick=function(){
-
-let elInput = document.getElementById("todo-input").value;
-    // coditions validate form
-    // si le champ d'input est vide
-if (elInput=="") {
-    alert("Ajoute une tâche")
-    
-} else if (elInput.length < 3) {
-    alert ("Tâche trop courte")
-    
-} else if(elInput.length > 35) {
-    alert ("Tâche trop longue")
-} else {
+document.getElementById("push").onclick = function () {
+  let elInput = document.getElementById("todo-input").value;
+  // coditions validate form
+  // si le champ d'input est vide
+  if (elInput == "") {
+    alert("Ajoute une tâche");
+  } else if (elInput.length < 3) {
+    alert("Tâche trop courte");
+  } else if (elInput.length > 35) {
+    alert("Tâche trop longue");
+  } else {
     console.log("Tout est ok");
     // selectionne la div là est les taches vont être affichées
-    document.getElementById("tasks").innerHTML +=
-    `<div class="d-flex justify-content-between align-items-center mb-3">
-     <p>${elInput}</p>
-     <button class="btn btn-danger">Delete</button>
-    </div>`
-     ;
-}     
+    document.getElementById(
+      "tasks"
+    ).innerHTML += `<div class="d-flex justify-content-between align-items-center mb-3 border-bottom py-3">
+     <span class="my-o taskname">${elInput}</span>
+     <button class="btn btn-danger delete">Delete</button>
+    </div>`;
+  }
+  // delete task
+  let tasksDelete = document.querySelectorAll(".delete");
+  for (let i = 0; i < tasksDelete.length; i++) {
+    tasksDelete[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
+  // task done
+  let tasksDone = document.querySelectorAll(".taskname");
+  for (let i = 0; i < tasksDone.length; i++) {
+    tasksDone[i].onclick = function () {
+      this.classList.toggle("completed");
+    };
+    document.querySelector("#todo-input").value = ""
+  }
 };
